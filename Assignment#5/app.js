2db9a18dd930446c83ca2cc71e50f730381cf2a8
BilalQamar63 <143898156+BilalQamar63@users.noreply.github.com>
2023-09-30 16:55:51 +0500

    Work is almost done

---
+++ Assignment#5/app.js
@@ -0,0 +1,69 @@
+// Chapter 21 ******* (Question 1)********
+
+
+/*var userInput=prompt();
+var allLower = userInput.toLowerCase();
+console.log(userInput);*/
+
+
+// Chapter 21 ******* (Question 2)********
+// var x = "Hello World";
+// x = x.toLowerCase();
+// console.log(x)
+
+// Chapter 21 ******* (Question 3)********
+
+// var y = "Hello World";
+// y = y.toUpperCase();
+// console.log(y)
+
+// Chapter 21 ******* (Question 4)********
+
+// var originalString = "Hello World"; 
+// var lowerCaseString = originalString.toLowerCase();
+
+// Chapter 21 ******* (Question 5)********
+// var myArray = ["Hello World"]; 
+// var lowerCaseElement = myArray[0].toLowerCase();
+
+// Chapter 21 ******* (Question 6)********
+
+// var myArr = ["Hello World"]
+// var lowerCaseElement2 = myArr[0].toLowerCase();
+// alert(lowerCaseElement2.toUpperCase());
+
+
+// Chapter 21 ******* (Question 7)********
+
+// var cityName = "lahore";
+// var capitalisedCityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+// console.log(capitalisedCityName);
+
+// Chapter 22-25 ******* (Question 1)********
+
+// var samewords = "captain";
+// var slicedportion = samewords.slice(1,3);
+// console.log(slicedportion);
+
+// Chapter 22-25 ******* (Question 2)********
+
+var sameWords = "captain";
+var numberOfCharacters = sameWords.length;
+var slicedPortion = sameWords.slice(1, numberOfCharacters - 1);
+console.log(slicedPortion); 
+
+// Chapter 22-25 ******* (Question 3)********
+// Chapter 22-25 ******* (Question 4)********
+// Chapter 22-25 ******* (Question 5)********
+// Chapter 22-25 ******* (Question 6)********
+// Chapter 22-25 ******* (Question 7)********
+// Chapter 22-25 ******* (Question 8)********
+// Chapter 22-25 ******* (Question 9)********
+// Chapter 22-25 ******* (Question 10)********
+// Chapter 22-25 ******* (Question 11)********
+// Chapter 22-25 ******* (Question 12)********
+// Chapter 22-25 ******* (Question 13)********
+// Chapter 22-25 ******* (Question 14)********
+// Chapter 22-25 ******* (Question 15)********
+// Chapter 22-25 ******* (Question 16)********
+// Chapter 22-25 ******* (Question 17)********
